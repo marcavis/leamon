e36192fe65745c3588278c839d6f47f6502842b7
marcavis <marcavis@yahoo.com.br>
2022-07-30 03:14:17 -0300

    temporarily make Karin a starter

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -118,7 +118,7 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_CYURIA,
-    SPECIES_TORCHIC,
+    SPECIES_CKARIN,
     SPECIES_MUDKIP,
 };
 
