298ec72cda30772fb769da6c29f400365c0c8e16
marcavis <marcavis@yahoo.com.br>
2022-08-11 00:48:19 -0300

    moe gopnik and office work

--- leamonScripts/moe-office.py
+++ leamonScripts/moe-office.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #do not put spaces between the variable and the value!
 #e.g. SPECIES=CYuria, not SPECIES = "CYuria" (the quotes are not needed)
-species="GMoe"
-images="leamon/moe-gopnik"
+species="OMoe"
+images="leamon/moe-office"
 animate=[(0,1)] #must be a list of tuples, like [(0,10),(1,20)]
 frontSpriteSize = (64,64)
 frontYOffset = 0
@@ -13,7 +13,7 @@ backAnim = "BACK_ANIM_H_VIBRATE"
 iconPalette = 0 #0, 1 or 2
 #don't put newlines in here
 pokedexText = [
-"You've come to the wrong turf!",
+"Busy, busy, busy!",
 "",
 "",
 ""]
@@ -28,21 +28,21 @@ trainerOffset=0
 
 
 baseHP=70
-baseAttack=90
+baseAttack=60
 baseDefense=80
-baseSpAttack=40
-baseSpDefense=50
-baseSpeed=70
+baseSpAttack=60
+baseSpDefense=80
+baseSpeed=50
 type1="TYPE_NORMAL"
-type2="TYPE_DARK"
+type2="TYPE_PSYCHIC"
 catchRate=150
 expYield=90
 evHP=0
-evAttack=1
+evAttack=0
 evDefense=1 
 evSpeed=0
 evSpAttack=0
-evSpDefense=0
+evSpDefense=1
 item1="ITEM_NONE"
 item2="ITEM_NONE"
 genderRatio="PERCENT_FEMALE(25)"
@@ -57,23 +57,19 @@ ability3="ABILITY_NONE"
 bodyColor="BODY_COLOR_BROWN"
 noFlip="FALSE"
 
-#dark allure level 30?
+#TODO: needs more moves
 learnset="""
 static const struct LevelUpMove s""" + species + """LevelUpLearnset[] = {
-    LEVEL_UP_MOVE( 1, MOVE_TACKLE),
-    LEVEL_UP_MOVE( 1, MOVE_CHARM),
-    LEVEL_UP_MOVE( 1, MOVE_BIDE),
-    LEVEL_UP_MOVE( 1, MOVE_ENDURE),
-    LEVEL_UP_MOVE( 8, MOVE_SWEET_KISS),
-    LEVEL_UP_MOVE(12, MOVE_STOMP),
-    LEVEL_UP_MOVE(16, MOVE_FALSE_SWIPE),
-    LEVEL_UP_MOVE(21, MOVE_FLAIL),
-    LEVEL_UP_MOVE(24, MOVE_WORK_UP),
-    LEVEL_UP_MOVE(29, MOVE_NATURAL_GIFT),
-    LEVEL_UP_MOVE(34, MOVE_RETURN),
-    LEVEL_UP_MOVE(37, MOVE_ENTRAINMENT),
-    LEVEL_UP_MOVE(42, MOVE_SWIFT),
-    LEVEL_UP_MOVE(48, MOVE_PLAY_ROUGH),
+    LEVEL_UP_MOVE( 1, MOVE_PAY_DAY),
+    LEVEL_UP_MOVE( 1, MOVE_GROWL),
+    LEVEL_UP_MOVE( 5, MOVE_MEDITATE),
+    LEVEL_UP_MOVE( 9, MOVE_COVET),
+    LEVEL_UP_MOVE(12, MOVE_YAWN),
+    LEVEL_UP_MOVE(16, MOVE_PSYBEAM),
+    LEVEL_UP_MOVE(21, MOVE_WORRY_SEED),
+    LEVEL_UP_MOVE(29, MOVE_ZEN_HEADBUTT),
+    LEVEL_UP_MOVE(36, MOVE_PAYBACK),
+    LEVEL_UP_MOVE(48, MOVE_DREAM_EATER),
     LEVEL_UP_END
 };"""
 
